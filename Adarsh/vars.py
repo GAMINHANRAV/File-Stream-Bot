c41d4a78a7036620c753712d47ccaacd9c6a2c14
GAMINHANRAV <129027559+GAMINHANRAV@users.noreply.github.com>
2023-10-28 18:16:59 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,13 +9,13 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '22497046'))
-    API_HASH = str(getenv('API_HASH', 'bb0627c9f42ed0e419a13deafb8ac210'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
+    API_ID = int(getenv('API_ID', '12774158'))
+    API_HASH = str(getenv('API_HASH', 'd7b49cf2022c84fdb4fafd71fa229be6'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '5888392970:AAELa3dn0H96FWmbQgMI1fL5748aN5Bq4KY'))
     name = str(getenv('name', 'filetolinkvjbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001902789595'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001857784827'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
